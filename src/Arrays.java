11e974d98b4e319e6308be70802dd1c27cbe7c79
Perfect User <me@PerfectUser.com>
2019-11-06 20:37:54 +0300

    Week 2 has ended

---
+++ src/Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int [] grades = new int [10];
+        grades[0] = 10;
+        System.out.println(grades[2]);
+
+    }
+}
---
+++ src/ForLoops.java
@@ -0,0 +1,17 @@
+public class ForLoops {
+    public static void main(String[] args) {
+
+        boolean additionalLogging = false;
+        for (int i = 9; i >= 0; i--){
+            System.out.println(i);
+
+            if (i == 5) {
+                if (additionalLogging == true); {
+                    System.out.println("We found...");
+                }
+            }
+
+
+        }
+    }
+}
