5a76580905f9ebc8118b148f8579e5c267f459b6
Perfect User <me@PerfectUser.com>
2020-03-07 13:29:28 -0500

    Custom Types as Arguments

--- src/Week3/SearchList.java
+++ src/Week3/SearchList.java
@@ -5,19 +5,24 @@ import java.util.List;
 
 public class SearchList {
     public static void main(String[] args) {
-String [] firstNames = {"Vasya", "Petya", "Ghenya", "Kostya"};
-String [] lastNames = {"Vasya1", "Petya1", "Ghenya1", "Kostya1"};
+        String[] firstNames = {"Vasya", "Petya", "Ghenya", "Kostya"};
+        String[] lastNames = {"Vasya1", "Petya1", "Ghenya1", "Kostya1"};
         List<User> users = new ArrayList<User>();
 
         for (int i = 0; i < firstNames.length; i++) {
             User u = new User();
-            u.setFirstName(firstNames [i]);
+            u.setFirstName(firstNames[i]);
             u.setLastName(lastNames[i]);
             users.add(u);
         }
-        for (User u:users) {
+        for (User u : users) {
             System.out.println(u.getFullName());
         }
+        SearchList searchList = new SearchList();
+        searchList.printUser(users.get(0));
+    }
 
+    public void printUser(User u){
+        System.out.println(u.getFullName());
     }
 }
