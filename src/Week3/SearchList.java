d2952c5a897192bc1c721b0ffab2b7ca6611ec15
Perfect User <me@PerfectUser.com>
2020-03-07 13:24:12 -0500

    Custom Types in Loop

--- src/Week3/SearchList.java
+++ src/Week3/SearchList.java
@@ -5,23 +5,19 @@ import java.util.List;
 
 public class SearchList {
     public static void main(String[] args) {
-        User me = new User();
-        me.setFirstName("Vasya");
-        me.setLastName("Petya");
-
-        User you = new User(); //local variable to the main method. Created an Object class
-        you.setFirstName("Sub");
-        you.setLastName("Scriber");
-
+String [] firstNames = {"Vasya", "Petya", "Ghenya", "Kostya"};
+String [] lastNames = {"Vasya1", "Petya1", "Ghenya1", "Kostya1"};
         List<User> users = new ArrayList<User>();
-        users.add(you);
-        users.add(me);
 
-        System.out.println(users.get(1).getFullName());
+        for (int i = 0; i < firstNames.length; i++) {
+            User u = new User();
+            u.setFirstName(firstNames [i]);
+            u.setLastName(lastNames[i]);
+            users.add(u);
+        }
+        for (User u:users) {
+            System.out.println(u.getFullName());
+        }
 
-//        System.out.println(user.getFullName());
-
-//        String message = user.output();
-//        System.out.println(message);
     }
 }
